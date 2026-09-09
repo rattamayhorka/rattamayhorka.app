7a7bbb17dd810ddef70bf764506b709ae7bb91b4
rattamayhorka <raymundo.cadena@gmail.com>
2026-09-08 18:40:26 -0600

    agrego trazos a mapa de proyectos

--- src/App.jsx
+++ src/App.jsx
@@ -543,7 +543,7 @@ export default function App() {
           </button>
           
           <div className="hidden xl:block text-center text-[9px] font-bold text-theme-text/50 tracking-widest mt-1">
-            rattamayhorka v2.0.9 "time deudas"
+            rattamayhorka v2.0.10 "trazos encimados"
           </div>
         </div>
       </div>
