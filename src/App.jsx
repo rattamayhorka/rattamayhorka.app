017657c562d034bf399b356dd64a55836f72cde4
rattamayhorka <raymundo.cadena@gmail.com>
2026-09-12 21:01:52 -0600

    corrijo varios bugs en el mapa

--- src/App.jsx
+++ src/App.jsx
@@ -543,7 +543,7 @@ export default function App() {
           </button>
           
           <div className="hidden xl:block text-center text-[9px] font-bold text-theme-text/50 tracking-widest mt-1">
-            rattamayhorka v2.0.11 "balazos programados"
+            rattamayhorka v2.0.12 "nodos necios"
           </div>
         </div>
       </div>
